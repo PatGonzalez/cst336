4ea678cd1e427634418e1f74437b8475a3e0a6b4
Patrick Gonzalez <patgonzalez@csumb.edu>
2018-06-16 22:29:44 +0000

    Second push

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 // Press the 'Run' button on the top to start the web server,
 // then click the URL that is emitted to the Output tab of the console
 
-echo 'Patrick Gonzalez's CST336 Internet Programming';
+echo "Patrick Gonzalez's CST336 Internet Programming";
 
 ?>
 </body>
